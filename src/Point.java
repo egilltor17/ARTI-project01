c658bf1ecc9fca0def5c23f4a3f03e951d7a9a6b
egilltor17 <egilltor17@ru.is>
2019-01-23 15:46:02 +0000

    added heuristic functions

--- src/Point.java
+++ src/Point.java
@@ -1,9 +1,19 @@
-public class Point{
-		public Point(int x, int y)
-		{
-			this.x = x;
-			this.y = y;
-		}
-		public int x;
-		public int y;
+public class Point
+{
+	public int x;
+	public int y;	
+
+	public Point(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
 	}
+	
+	public int manhatanDist(Point p)
+	{
+		int xx = Math.abs(this.x - p.x);
+		int yy = Math.abs(this.x - p.y);
+		if((xx == 0) || (yy == 0)) return xx + yy;
+		return xx + yy + 1;
+	}
+}
