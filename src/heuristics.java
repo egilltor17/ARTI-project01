85fbed0e3743d88c355bfcb8cc54a5448bd61ab1
egilltor17 <egilltor17@ru.is>
2019-01-25 16:35:14 +0000

    Small heurstic refactor

--- src/heuristics.java
+++ src/heuristics.java
@@ -9,7 +9,7 @@ public class heuristics {
 	 */
 	public int[] manhatanHeuristic(State state, Point[] dirtPos, Point homePos)
 	{	
-		int distGo[] = new int[state.dirts.length];
+		int distFront[] = new int[state.dirts.length];
 		int distLeft[] = new int[state.dirts.length];
 		int distRight[] = new int[state.dirts.length];
 		boolean noDirtsLeft = true;
@@ -50,8 +50,8 @@ public class heuristics {
 			{
 				if(state.dirts[i]) 
 				{
-					distGo[i] = 100000;
-					distLeft[i] = 100000;
+					distFront[i] = 100000;
+					distLeft[i]  = 100000;
 					distRight[i] = 100000;
 				} 
 				else 
@@ -62,34 +62,34 @@ public class heuristics {
 					int W = dirtPos[i].manhatanDist(new Point(state.posx - 1, state.posy));
 					if(state.orientation == 'N' ) 
 					{
-						distGo[i] = N;
-						distLeft[i] = W + 1;
+						distFront[i] = N;
+						distLeft[i]  = W + 1;
 						distRight[i] = E + 1;
 					} 
 					else if(state.orientation == 'E' ) 
 					{
-						distGo[i] = E;
-						distLeft[i] = N + 1;
+						distFront[i] = E;
+						distLeft[i]  = N + 1;
 						distRight[i] = S + 1;
 					} 
 					else if(state.orientation == 'S' ) 
 					{
-						distGo[i] = S;
-						distLeft[i] = E + 1;
+						distFront[i] = S;
+						distLeft[i]  = E + 1;
 						distRight[i] = W + 1;
 					} 
 					else 
 					{
-						distGo[i] = W;
-						distLeft[i] = S + 1;
+						distFront[i] = W;
+						distLeft[i]  = S + 1;
 						distRight[i] = N + 1;
 					}
 				}
 			}
-			Arrays.sort(distGo);
+			Arrays.sort(distFront);
 			Arrays.sort(distLeft);
 			Arrays.sort(distRight);
-			return new int[] {distLeft[0], distGo[0], distRight[0]};
+			return new int[] {distLeft[distLeft.length - 1], distFront[distFront.length - 1], distRight[distRight.length - 1]};
 		}
 	}
 	
